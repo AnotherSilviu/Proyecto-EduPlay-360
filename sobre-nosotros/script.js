69c3ec5841b23c0ef8d547041a5bff73505e747b
AnotherSilviu <silviaes1997@gmail.com>
2025-05-08 21:03:37 +0200

    añadido css y paginas casi a punto

--- sobre-nosotros/script.js
+++ sobre-nosotros/script.js
@@ -1,7 +1,7 @@
 function mostrarFunciones(nombre) {
     const funciones = {
       "Ramón García": "<h3>Ramón García</h3><p><strong>Funciones Clave:</strong> Supervisión de la estrategia global, alineación con socios y gestión del presupuesto.</p><p><strong>Fases ADDIE:</strong> TODAS (Análisis y Evaluación en especial)</p>",
-      "Raquel Enriquez": "<h3>Raquel Enriquez</h3><p><strong>Funciones Clave:</strong>Gestión pedagógica, supervisión de la calidad educativa y del cumplimiento de los objetivos de la formación.</p><p><strong>Fases ADDIE:</strong>Diseño, Desarrollo, Implementación</p>",
+      "Raquel Enrique": "<h3>Raquel Enrique</h3><p><strong>Funciones Clave:</strong>Gestión pedagógica, supervisión de la calidad educativa y del cumplimiento de los objetivos de la formación.</p><p><strong>Fases ADDIE:</strong>Diseño, Desarrollo, Implementación</p>",
       "Carolina Márquez": "<h3>Carolina Márquez</h3><p><strong>Funciones Clave:</strong>Diseño del itinerario (ADDIE), gamificación, accesibilidad (DUA)</p><p><strong>Fases ADDIE:</strong>Análisis, Diseño, Desarrollo</p>",
       "M. Teresa Ventanilla": "<h3>M. Teresa Ventanilla</h3><p><strong>Funciones Clave:</strong>Diseño de talleres y acompañamiento práctico.</p><p><strong>Fases ADDIE:</strong>Desarrollo, Implementación, Evaluación</p>",
       "Luis Fernández": "<h3>Luis Fernández</h3><p><strong>Funciones Clave:</strong> Diseño de talleres y acompañamiento práctico.</p><p><strong>Fases ADDIE:</strong> Desarrollo, Implementación</p>",
