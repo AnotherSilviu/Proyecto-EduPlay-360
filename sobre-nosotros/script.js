4f9f688b123d33d7e35fe5c483615a053c760631
AnotherSilviu <silviaes1997@gmail.com>
2025-05-16 15:21:29 +0200

    creado todo el esquema de la página

--- sobre-nosotros/script.js
+++ sobre-nosotros/script.js
@@ -25,3 +25,18 @@ function mostrarFunciones(nombre) {
     const grid = header.nextElementSibling;
     grid.classList.toggle("hidden");
   }
+
+  function toggleDept(header) {
+  const grid = header.nextElementSibling;
+  grid.classList.toggle("hidden");
+}
+function scrollToInfo() {
+    document.getElementById('info').scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    const animatables = document.querySelectorAll('.animate-in');
+    animatables.forEach((el, i) => {
+      el.style.animationDelay = `${i * 0.3}s`;
+    });
+  });
